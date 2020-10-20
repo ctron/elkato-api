d4627a5cf8947577218b96f5fa66bf6aa5783096
Jens Reimann <jreimann@redhat.com>
2020-10-20 20:20:36 +0200

    Show most recent, with selection

--- elkato-frontend/src/lib.rs
+++ elkato-frontend/src/lib.rs
@@ -8,7 +8,12 @@ use elkato_common::data::*;
 
 use headers::authorization::Credentials;
 use headers::Authorization;
+
 use wasm_bindgen::prelude::*;
+
+use chrono_tz::Europe::Berlin;
+
+use chrono::Utc;
 use yew::format::{Json, Nothing};
 use yew::prelude::*;
 use yew::services::fetch::{FetchService, FetchTask, Request, Response};
@@ -16,12 +21,39 @@ use yew::services::fetch::{FetchService, FetchTask, Request, Response};
 struct Model {
     link: ComponentLink<Self>,
     ft: Option<FetchTask>,
-    bookings: Vec<Booking>,
+    bookings: Vec<Selected<Booking>>,
 }
 
 pub enum Msg {
     FetchData(),
     FetchReady(Result<Vec<Booking>, Error>),
+}
+
+#[derive(Debug)]
+pub struct Selected<T> {
+    pub value: T,
+    pub selected: bool,
+}
+
+impl<T> Selected<T> {
+    pub fn new(value: T) -> Self {
+        Selected {
+            value,
+            selected: false,
+        }
+    }
+}
+
+impl<T> Clone for Selected<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        Selected {
+            selected: self.selected,
+            value: self.value.clone(),
+        }
+    }
 }
 
 impl Component for Model {
@@ -43,7 +75,13 @@ impl Component for Model {
         match msg {
             Msg::FetchData() => self.ft = Some(self.fetch_update()),
             Msg::FetchReady(response) => {
-                self.bookings = response.unwrap_or_default();
+                self.bookings = self.select(
+                    response
+                        .unwrap_or_default()
+                        .iter()
+                        .map(|b| Selected::new(b.clone()))
+                        .collect(),
+                );
             }
         }
         true
@@ -63,17 +101,22 @@ impl Component for Model {
                 </PageSection>
                 <PageSection>
                     <Gallery gutter=true>
-                        { for self.bookings.iter().map(|booking|{
+                        { for self.bookings.iter().map(|sel_booking|{
                             html_nested!{
 
                         <Card
                             selectable=true
+                            selected={sel_booking.selected}
                             title={html_nested!{<>
-                                { &booking.start } {" / "} { &booking.end }
+                                { self.title(&sel_booking.value) }
                             </>}}
                             >
-                            <div>{ &booking.id } {" / "} { &booking.resource }</div>
-                            <div>{ booking.description.as_ref().map(|s|s.clone()).unwrap_or_default() }</div>
+                            <div>{ &sel_booking.value.resource }</div>
+                            { for sel_booking.value.description.iter().map(|desc|{
+                                html_nested!{
+                                    <div>{ desc }</div>
+                                }
+                            })}
 
                         </Card>
 
@@ -119,6 +162,75 @@ impl Model {
         log::info!("FT: {:?}", ft);
 
         ft.unwrap()
+    }
+
+    fn title(&self, booking: &Booking) -> String {
+        format!(
+            "{} -> {}",
+            booking
+                .start
+                .with_timezone(&Berlin)
+                .format("%d-%m-%Y %H:%M"),
+            booking.end.with_timezone(&Berlin).format("%d-%m-%Y %H:%M")
+        )
+    }
+
+    fn select(&self, mut bookings: Vec<Selected<Booking>>) -> Vec<Selected<Booking>> {
+        // sort by time asc
+
+        bookings.sort_by(|a, b| {
+            a.value
+                .start
+                .cmp(&b.value.start)
+                .then_with(|| a.value.end.cmp(&b.value.end))
+        });
+
+        // back to front
+
+        log::info!("Bookings: {:?}", bookings);
+
+        let now = Utc::now();
+        let mut selected: Option<&String> = None;
+
+        for b in bookings.iter().rev() {
+            log::info!("Sel: {:?}", b);
+            if b.value.start > now {
+                selected = Some(&b.value.id);
+            } else {
+                if selected.is_none() {
+                    selected = Some(&b.value.id);
+                }
+                break;
+            }
+        }
+
+        let selected = selected.map(|s| s.clone());
+
+        if let Some(sel) = selected {
+            for ref mut b in &mut bookings {
+                if b.value.id.eq(&sel) {
+                    b.selected = true;
+                }
+            }
+
+            let mut new = Vec::new();
+
+            for b in bookings.iter().rev() {
+                let mut b = b.clone();
+
+                if b.value.id.eq(&sel) {
+                    b.selected = true;
+                    new.push(b);
+                    new.reverse();
+                    return new;
+                } else {
+                    new.push(b);
+                }
+            }
+            new
+        } else {
+            bookings
+        }
     }
 }
 
